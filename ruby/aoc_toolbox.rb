aefbb42ba3d55c2bfb92bd19fbcd6e07fa0a3a66
Robert Buren <robert@buren.se>
2021-11-22 19:30:20 +0100

    ruby: InputFile now returns an InputFile instance for sections and sub_matrix

--- ruby/aoc_toolbox.rb
+++ ruby/aoc_toolbox.rb
@@ -1,28 +1,23 @@
 class InputFile
-    def initialize(filename='input.txt')
-        @filename = filename
-        @raw_lines = []
-    end
-
-    private
-    # returns an array of raw file content, each line as one string. 
-    def raw_lines(strip_newline=true)
-        if @raw_lines == []
+    def initialize(filename: 'input.txt', lines: nil, strip_newline: true)
+        if (lines)
+            @raw_lines = lines.dup
+        else
+            @filename = filename
             @raw_lines = File.open(@filename).readlines()
-            @raw_lines.map{|line| line.strip} if strip_newline
+            @raw_lines.map!{|line| line.strip} if strip_newline
         end
-        @raw_lines
     end
 
     public
     # returns an array of lines as strings. each line is stripped from ending newline char.
     def lines
-        self.raw_lines
+        @raw_lines
     end
 
     # returns an array of lines as integers
     def lines_ints
-        self.raw_lines.map(&:to_i)
+        self.lines.map(&:to_i)
     end
 
     # returns an array of lines, each containing an array of 
@@ -32,21 +27,21 @@ class InputFile
     end
 
     # returns an array of sections, as identified by the existance of 
-    # empty lines in the input data. Each section contains a number of 
-    # lines as string in an array for each section.
+    # empty lines in the input data. Each section is a InputFile instance containing 
+    # the lines for each section.
     def sections
         sections = []
         current_section = []
-        lines.each do |line|
+        self.lines.each do |line|
             if line == ''
-                sections.append(current_section) unless current_section.empty?
+                sections.append(InputFile.new(lines: current_section)) unless current_section.empty?
                 current_section = []
             else
                 current_section.append(line)
             end
         end
         # deal with last line if non-empty
-        sections.append(current_section) unless current_section.empty?
+        sections.append(InputFile.new(lines: current_section)) unless current_section.empty?
         sections
     end
 
@@ -54,12 +49,13 @@ class InputFile
         self.lines[row][col]
     end
 
+    # returns a InputFile instance representing the sub matrix pointed to by the parameters
     def sub_matrix(start_row, start_col, rows, cols)
         sub = []
-        for row in lines[start_row...(start_row+rows)]
+        for row in self.lines[start_row...(start_row+rows)]
             sub_row = row[start_col...(start_col+cols)]
             sub.append(sub_row)
         end
-        sub
+        InputFile.new(lines: sub)
     end
 end
--- ruby/test/aoc_toolbox_test.rb
+++ ruby/test/aoc_toolbox_test.rb
@@ -15,24 +15,23 @@ class AocToolboxTest < Test::Unit::TestCase
     end
 
     def test_fields_regexp
-        aoc = InputFile.new("input-fields.txt")
+        aoc = InputFile.new(filename: "input-fields.txt")
         assert(aoc.linefields_regexp(/(\w+): (\d+)\s(\d+)\s(\d+)/)[0] == ['kalle', '123','456','879'])
     end
 
     def test_sections
-        aoc = InputFile.new("input-sections.txt")
-        assert(aoc.sections[3] == ['section 4', 'banana'])
+        aoc = InputFile.new(filename: "input-sections.txt")
+        assert(aoc.sections[3].lines == ['section 4', 'banana'])
     end
 
     def test_matrix_char
-        aoc = InputFile.new("input-matrix.txt")
+        aoc = InputFile.new(filename: "input-matrix.txt")
         assert(aoc.matrix_char(1, 4) == '.')
         assert(aoc.matrix_char(3, 15) == '#')
     end
 
     def test_sub_matrix
-        aoc = InputFile.new("input-matrix.txt")
-        puts aoc.sub_matrix(1,1,2,2).to_s
-        assert(aoc.sub_matrix(1,1,2,2) == ['.#','..'])
+        aoc = InputFile.new(filename: "input-matrix.txt")
+        assert(aoc.sub_matrix(1,1,2,2).lines == ['.#','..'])
     end
 end
