771cff5d457e26bfd0d25ca3b922989111fe6906
Robert Buren <robert@buren.se>
2021-11-23 16:30:47 +0100

    added pre-aoc test version of day01

---
+++ day01/aoc_toolbox.js
@@ -0,0 +1,98 @@
+const fs = require('fs')
+
+class InputData {
+    constructor(params={}) {
+        if (params.lines) {
+            this._lines = params.lines
+        } else {
+            let filename = params.filename || "input.txt"
+            this._lines = fs.readFileSync(filename).toString().trim().split("\n")
+        }
+    }
+
+    /**
+     * 
+     * @returns cached file content as an array, containing each line in the file.
+     */
+    lines() {
+        return this._lines
+    }
+
+    /**
+     * 
+     * @returns same as InputData.lines() but each line converted to integer
+     */
+    linesInts() {
+        return this.lines().map((x) => parseInt(x))
+    }
+
+    /**
+     * 
+     * @param {RegExp} r matching and identifying the fields of each line
+     * @returns an array with an element for each line, each being an array with all the fields matched by the supplied RegExp
+     */
+    linefieldsRegexp(r) {
+        return this.lines().map((line) => {
+            let matches = line.match(r)
+            matches.shift()
+            return matches
+        })
+    }
+
+    /**
+     * A section in the input data is defined as a number of consecutive lines without any ampty lines
+     * between them. One or more consecutive empty lines separates each section (but is not considered to
+     * be part of any section).
+     * @returns an array with each section, represented as a new InputData instance
+     */
+    sections() {
+        let sections = []
+        let currentSection = []
+        let ls = this.lines()
+        for (let lineNum in ls) {
+            if (ls[lineNum] == '') {
+                if (currentSection.length > 0) {
+                    sections.push(new InputData({lines: currentSection}))
+                }
+                currentSection = []
+            } else {
+                currentSection.push(ls[lineNum])
+            }
+        }
+        if (currentSection.length > 0) {
+            sections.push(new InputData({lines: currentSection}))
+        }
+        return sections
+    }
+
+    /**
+     * Considering the input file as a matrix of characters, this method access a specific character
+     * by its coordinates.
+     * @param {number} row 
+     * @param {number} col 
+     * @returns the character in the matrix at the supplied coordinates
+     */
+    matrixChar(row, col) {
+        return this.lines()[row][col]
+    }
+
+    /**
+     * Considering the input file as a matrix of characters, this method extracts a sub matrix from the original.
+     * @param {number} startRow 
+     * @param {number} startCol 
+     * @param {number} rows 
+     * @param {number} cols 
+     * @returns the sub matrix, as a new InputData instance
+     */
+    subMatrix(startRow, startCol, rows, cols) {
+        let sub = []
+        let ls = this.lines()
+        for (let rowNum = startRow; rowNum < startRow + rows; rowNum++) {
+            let subRow = ls[rowNum].substr(startCol, cols)
+            sub.push(subRow)
+        }
+        return new InputData({lines: sub})
+    }
+}
+
+module.exports = {InputData}
---
+++ day01/index.js
@@ -0,0 +1,43 @@
+const { isPrimitive } = require('util')
+const {InputData} = require('./aoc_toolbox')
+
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+
+const part1 = () => {
+//    let ints = new InputData({filename: "input-test1.txt"}).linesInts()
+    let ints = new InputData().linesInts()
+    let total = 0
+    for (let i=0; i<ints.length; i++) {
+        if (isPrime(ints[i])) {
+            total += i * ints[i]
+        }
+    }
+    return total
+}
+
+const part2 = () => {
+//    let ints = new InputData({filename: "input-test2.txt"}).linesInts()
+    let ints = new InputData().linesInts()
+    let total = 0
+    for (let i=0; i<ints.length; i++) {
+        if (!isPrime(ints[i])) {
+            if (i % 2 == 0) {
+                total += ints[i]
+            } else {
+                total -= ints[i]
+            }    
+        }
+    }
+    return total
+}
+
+const part = process.env.part || "part1"
+
+if (part === "part1")
+    console.log(part1())
+else
+    console.log(part2())
