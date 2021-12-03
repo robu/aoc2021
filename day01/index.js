ce7fa6bb2c40e5b13ae82d12aac1165cf946f27b
Robert Buren <robert@buren.se>
2021-12-03 17:24:43 +0100

    simplified day01

--- day01/index.js
+++ day01/index.js
@@ -1,12 +1,7 @@
 const { InputData, countCompare, equidistantPairs } = require('aoc-toolbox')
 const input = new InputData()
 
-const part1 = () => {
-    return countCompare(input.linesInts(), (x, y) => x < y)
-}
-
-const part2 = () => {
-    return equidistantPairs(input.linesInts(), 3).filter((elem) => elem[0] < elem[1]).length
-}
+const part1 = () => countCompare(input.linesInts(), (x, y) => x < y)
+const part2 = () => equidistantPairs(input.linesInts(), 3).filter(([x, y]) => x < y).length
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
