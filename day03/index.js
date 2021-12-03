cf09743399fff67ab00df4583fe408f33065f860
Robert Buren <robert@buren.se>
2021-12-03 11:29:24 +0100

    simplified

--- day03/index.js
+++ day03/index.js
@@ -1,15 +1,12 @@
-const { InputData, countCompare, equidistantPairs } = require('aoc-toolbox')
+const { InputData, cs } = require('aoc-toolbox')
 const input = new InputData()
 
 const part1 = () => {
     const rows = input.lines().length
     const cols = input.lines()[0].length
     let counters = []
-    for (let i = 0; i < cols; i++) { counters.push(0) } // count "1s" for each column
     for (let col = 0; col < cols; col++) {
-        for (let row = 0; row < rows; row++) {
-            if (input.matrixChar(row, col) == '1') { counters[col] += 1 }
-        }
+        counters.push(input.countOccurrencesInColumn(col, '1'))
     }
     let gammaStr = ''
     let epsilonStr = ''
