a036d350442adf3b856a54c575f6dc93ccd7c267
Robert Buren <robert@buren.se>
2021-12-05 09:13:47 +0100

    day05: initial checkin

---
+++ day05/index.js
@@ -0,0 +1,12 @@
+const { InputData, cs } = require('aoc-toolbox')
+const input = new InputData()
+
+const part1 = () => {
+    return -1
+}
+
+const part2 = () => {
+    return -1
+}
+
+console.log((process.env.part || "part1") == "part1" ? part1() : part2())
