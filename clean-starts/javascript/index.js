bd17844f191a759126394b99b1d0748f376baf6a
Robert Buren <robert@buren.se>
2021-12-05 13:49:59 +0100

    added clean startup files for javascript

---
+++ clean-starts/javascript/index.js
@@ -0,0 +1,12 @@
+const { InputData, cs } = require('aoc-toolbox')
+const input = new InputData({lines: ["test1", "test2"]})
+
+const part1 = () => {
+    return -1
+}
+
+const part2 = () => {
+    return -1
+}
+
+console.log((process.env.part || "part1") == "part1" ? part1() : part2())
