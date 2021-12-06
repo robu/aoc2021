e16871784fb5eec20dffccd3335c4b3b0f96e4ae
Robert Buren <robert@buren.se>
2021-12-06 12:12:10 +0100

    day06: reduced lines by removing ceremony

--- day06/index.js
+++ day06/index.js
@@ -1,28 +1,19 @@
-const fs = require('fs')
-const input = fs.readFileSync('input.txt', { encoding: 'utf8' }).trim().split("\n")[0].split(',').map((x) => +x)
+const input = require('fs').readFileSync('input.txt', { encoding: 'utf8' }).trim().split("\n")[0].split(',').map((x) => +x)
 
-class FishGenerations {
-    constructor(fishList = [], range = 9, resetTo = 6) {
-        this._range = range
-        this._resetTo = resetTo
-        this._generationCounters = new Array(this._range).fill(0)
-        fishList.forEach((f) => this.addFish(1, f))
+let genCounters = new Array(9).fill(0)
+input.forEach((f) => genCounters[f] += 1)
+
+const increaseDays = (days) => {
+    for (let i = 0; i < days; i++) {
+        let mothers = genCounters.shift()
+        genCounters.push(0)
+        genCounters[8] += mothers
+        genCounters[6] += mothers
     }
-
-    addFish = (count, generation) => this._generationCounters[generation] += count
-
-    increaseDays(iterations = 1) {
-        for (let i = 0; i < iterations; i++) {
-            let mothers = this._generationCounters.shift()
-            this._generationCounters.push(0)
-            this.addFish(mothers, this._range - 1) // children added to the end of the queue
-            this.addFish(mothers, this._resetTo) // previous mothers added to the reset generation
-        }
-        return this._generationCounters.reduce((x, y) => x + y)
-    }
+    return genCounters.reduce((x, y) => x + y)
 }
 
-const part1 = () => new FishGenerations(input).increaseDays(80)
-const part2 = () => new FishGenerations(input).increaseDays(256)
+const part1 = () => increaseDays(80)
+const part2 = () => increaseDays(256)
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
