ecc6e492dc41336efbb4b587b5500ca8f6e7620e
Robert Buren <robert@buren.se>
2021-12-06 22:51:17 +0100

    day02: new solution

--- day02/index.js
+++ day02/index.js
@@ -1,33 +1,28 @@
-const { InputData, countCompare, equidistantPairs } = require('aoc-toolbox')
-const input = new InputData()
+const input = require('fs').readFileSync('input.txt', { encoding: 'utf8' }).trim().split('\n').map((line) => {
+    [command, value] = line.split(' ')
+    return { command, value: +value }
+})
 
 const part1 = () => {
-    let depth = horizontal = 0
-    input.linefieldsSeparator(' ').forEach(([cmd, magnitude]) => {
-        if (cmd == 'forward') {
-            horizontal += parseInt(magnitude)
-        } else if (cmd == 'up') {
-            depth -= parseInt(magnitude)
-        } else if (cmd == 'down') {
-            depth += parseInt(magnitude)
-        }
-    })
-    return horizontal * depth
+    let machine = {
+        forward: ({ position, depth }, value) => { return { position: position + value, depth } },
+        up: ({ position, depth }, value) => { return { position, depth: depth - value } },
+        down: ({ position, depth }, value) => { return { position, depth: depth + value } },
+    }
+    let state = { depth: 0, position: 0 }
+    input.forEach(({ command, value }) => { state = machine[command](state, value) })
+    return state.position * state.depth
 }
 
 const part2 = () => {
-    let depth = horizontal = aim = 0
-    input.linefieldsSeparator(' ').forEach(([cmd, magnitude]) => {
-        if (cmd == 'forward') {
-            horizontal += parseInt(magnitude)
-            depth += parseInt(magnitude) * aim
-        } else if (cmd == 'up') {
-            aim -= parseInt(magnitude)
-        } else if (cmd == 'down') {
-            aim += parseInt(magnitude)
-        }
-    })
-    return horizontal * depth
+    let machine = {
+        forward: ({ position, depth, aim }, value) => { return { position: position + value, depth: depth + aim * value, aim } },
+        up: ({ position, depth, aim }, value) => { return { position, depth, aim: aim - value } },
+        down: ({ position, depth, aim }, value) => { return { position, depth, aim: aim + value } },
+    }
+    let state = { depth: 0, position: 0, aim: 0 }
+    input.forEach(({ command, value }) => { state = machine[command](state, value) })
+    return state.position * state.depth
 }
 
 console.log((process.env.part || "part1") == "part1" ? part1() : part2())
